c8ac7ed0256b7ef38dabc2f7e976e373798c351c
jwpark1211 <1211abc@naver.com>
2025-03-12 23:41:48 +0900

    feat: RESTful 규칙을 준수하도록 Comment API 엔드포인트 수정

--- src/main/java/capstone/bookitty/domain/comment/api/CommentApi.java
+++ src/main/java/capstone/bookitty/domain/comment/api/CommentApi.java
@@ -17,21 +17,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name="코멘트", description = "코멘트 관리 api 입니다.")
+@Tag(name="comment", description = "댓글 관리 api 입니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comment")
+@RequestMapping("/comments")
 public class CommentApi {
 
     private final CommentService commentService;
 
     @Operation(summary = "코멘트 생성 / content는 최소 1자, 최대 100자")
-    @PostMapping(path = "/new")
-    public ResponseEntity<IdResponse> saveComment(
+    @PostMapping
+    public ResponseEntity<IdResponse> createComment(
             @RequestBody @Valid final CommentSaveRequest request
     ){
         IdResponse response = commentService.saveComment(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(201).body(response); //201 created
     }
 
     @Operation(summary = "commentId로 코멘트 가져오기")
@@ -40,82 +40,84 @@ public class CommentApi {
             @PathVariable("comment-id") Long commentId
     ){
         CommentInfoResponse response = commentService.findCommentByCommentId(commentId);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
-    @Operation(summary = "전체 코멘트 가져오기 / page는 requestParam으로 요청할 수 있습니다. / "+
-                        "size(한 페이지 당 element 수, default = 10), page(요청하는 페이지, 0부터 시작)")
-    @GetMapping(path = "/all")
+    @Operation(summary = "전체 코멘트 가져오기 (페이지네이션 지원)")
+    @GetMapping
     public ResponseEntity<Page<CommentInfoResponse>> getAll(
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ){
         Page<CommentInfoResponse> responseList = commentService.findAllComment(pageable);
-        return ResponseEntity.ok().body(responseList);
+        return ResponseEntity.ok(responseList);
     }
 
-    @Operation(summary = "isbn으로 코멘트 리스트 가져오기 / page는 requestParam으로 요청할 수 있습니다. / "+
-            "size(한 페이지 당 element 수, default = 10), page(요청하는 페이지, 0부터 시작)")
+    @Operation(summary = "isbn으로 코멘트 리스트 가져오기")
     @GetMapping(path = "/isbn/{isbn}")
     public ResponseEntity<Page<CommentInfoResponse>> getCommentByISBN(
             @PathVariable("isbn") String isbn,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ){
         Page<CommentInfoResponse> responseList = commentService.findCommentByIsbn(isbn,pageable);
-        return ResponseEntity.ok().body(responseList);
+        return ResponseEntity.ok(responseList);
     }
 
-    @Operation(summary = "memberId로 코멘트 리스트 가져오기 / page는 requestParam으로 요청할 수 있습니다. / "+
-            "size(한 페이지 당 element 수, default = 10), page(요청하는 페이지, 0부터 시작)")
-    @GetMapping(path = "/member/{member-id}")
+    @Operation(summary = "memberId로 코멘트 리스트 가져오기")
+    @GetMapping(path = "/members/{member-id}")
     public ResponseEntity<Page<CommentInfoResponse>> getCommentByMemberId(
             @PathVariable("member-id") Long memberId,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ){
         Page<CommentInfoResponse> responseList = commentService.findCommentByMemberId(memberId, pageable);
-        return ResponseEntity.ok().body(responseList);
+        return ResponseEntity.ok(responseList);
     }
 
     @Operation(summary = "코멘트 수정 / content는 최소 1자, 최대 100자")
     @PatchMapping(path = "/{comment-id}")
-    public void updateComment(
+    public ResponseEntity<Void> updateComment(
             @PathVariable("comment-id") Long commentId,
             @RequestBody @Valid CommentUpdateRequest request
     ){
         commentService.updateComment(commentId, request);
+        return ResponseEntity.noContent().build(); //204 No Content;
     }
 
     @Operation(summary = "코멘트 삭제")
     @DeleteMapping(path = "/{comment-id}")
-    public void deleteComment(
+    public ResponseEntity<Void> deleteComment(
             @PathVariable("comment-id") Long commentId
     ){
         commentService.deleteComment(commentId);
+        return ResponseEntity.noContent().build(); //204 No Content;
     }
 
     @Operation(summary = "코멘트 좋아요 등록")
-    @PostMapping(path = "/{comment-id}/member/{member-id}/like/increase")
-    public void increaseLike(
+    @PostMapping(path = "/{comment-id}/likes")
+    public ResponseEntity<Void> likeComment(
             @PathVariable("comment-id") Long commentId,
             @PathVariable("member-id") Long memberId
     ){
         commentService.increaseLike(commentId,memberId);
+        return ResponseEntity.status(201).build(); //201 Created
     }
 
     @Operation(summary = "코멘트 좋아요 삭제")
-    @PostMapping(path = "/{comment-id}/member/{member-id}/like/decrease")
-    public void decreaseLike(
+    @DeleteMapping(path = "/{comment-id}/likes")
+    public ResponseEntity<Void> decreaseLike(
             @PathVariable("comment-id") Long commentId,
             @PathVariable("member-id") Long memberId
     ){
         commentService.decreaseLike(commentId,memberId);
+        return ResponseEntity.noContent().build(); //204 No Content;
     }
 
     @Operation(summary = "좋아요 id로 삭제")
-    @DeleteMapping(path = "/like/{like-id}")
-    public void deleteLike(
+    @DeleteMapping(path = "/likes/{like-id}")
+    public ResponseEntity<Void> deleteLike(
             @PathVariable("like-id") Long likeId
     ){
         commentService.deleteLike(likeId);
+        return ResponseEntity.noContent().build(); //204 No Content;
     }
 
 }
