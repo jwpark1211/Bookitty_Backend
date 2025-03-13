2d4503f5096e6880ee10ed88aff537d0e1eb1520
jwpark1211 <1211abc@naver.com>
2025-03-13 17:35:49 +0900

    feat: RESTful 규칙을 준수하도록 Member API 엔드포인트 수정

--- src/main/java/capstone/bookitty/domain/member/api/MemberApi.java
+++ src/main/java/capstone/bookitty/domain/member/api/MemberApi.java
@@ -19,7 +19,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "회원", description = "회원 관련 api 입니다.")
+@Tag(name = "member", description = "회원 관련 api 입니다.")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -28,22 +28,21 @@ public class MemberApi {
 
     private final MemberService memberService;
 
-    @Operation(summary = "회원가입/"+"비밀번호=영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자/"
-    +"gender=MALE or FEMALE/"+"birthdate=yyyy-mm-dd/"+"email=이메일 format 준수")
-    @PostMapping(path = "/new")
-    public ResponseEntity<IdResponse> save(
+    @Operation(summary = "회원가입")
+    @PostMapping
+    public ResponseEntity<IdResponse> createMember(
             @RequestBody @Valid MemberSaveRequest request){
         IdResponse response = memberService.saveMember(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(201).body(response);
     }
 
     @Operation(summary = "이메일 중복 확인")
-    @GetMapping(path = "/email/{email}/unique")
+    @GetMapping("/email/unique")
     public ResponseEntity<BoolResponse> isEmailUnique(
-            @PathVariable("email") String email
+            @RequestParam("email") String email
     ){
         BoolResponse response = memberService.isEmailUnique(email);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "로그인")
@@ -52,7 +51,7 @@ public class MemberApi {
             @RequestBody @Valid MemberLoginRequest request
     ){
         TokenResponse response = memberService.login(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "토큰 재발행")
@@ -61,58 +60,58 @@ public class MemberApi {
             @RequestBody @Valid TokenRequest request
     ){
         TokenResponse response = memberService.reissue(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "로그인 한 회원 정보 조회")
     @GetMapping("/me")
     public ResponseEntity<MemberInfoResponse> getMyMemberInfo(){
-        log.info("api entry");
         MemberInfoResponse response = memberService.getMyInfo();
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
-    @Operation(summary = "로그아웃") /* 삭제 고민*/
+    @Operation(summary = "로그아웃")
     @PostMapping("/logout")
-    public void logout(@RequestBody TokenRequest tokenRequest) {
+    public ResponseEntity<Void> logout(@RequestBody TokenRequest tokenRequest) {
         memberService.logout(tokenRequest);
+        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "id로 회원 조회")
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{member-id}")
     public ResponseEntity<MemberInfoResponse> findOneMember(
-            @PathVariable("id") Long memberId
+            @PathVariable("member-id") Long memberId
     ){
         MemberInfoResponse response = memberService.getMemberInfoWithId(memberId);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }
 
-    @Operation(summary = "전체 회원 조회 / page는 requestParam으로 요청할 수 있습니다. / "+
-    "size(한 페이지 당 element 수, default = 10), page(요청하는 페이지, 0부터 시작)")
-    @GetMapping(path = "/all")
+    @Operation(summary = "전체 회원 조회")
+    @GetMapping
     public ResponseEntity<Page<MemberInfoResponse>> findAllMembers(
             @PageableDefault(sort="id",size = 10) Pageable pageable
     ){
         Page<MemberInfoResponse> responseList = memberService.getAllMemberInfo(pageable);
-        return ResponseEntity.ok().body(responseList);
+        return ResponseEntity.ok(responseList);
     }
 
     /*@Operation(summary = "회원 프로필 업로드 / requestPart 이름 : profile")
-    @PostMapping(path = "/{id}/profile")
+    @PutMapping(path = "/{member-id}/profile")
     public ResponseEntity<MemberInfoResponse> updateMemberProfile(
-            @PathVariable("id") Long memberId,
+            @PathVariable("member-id") Long memberId,
             @RequestPart(value = "profile") MultipartFile profileImg
             ) throws IOException {
         MemberInfoResponse response = memberService.updateProfile(memberId, profileImg);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok(response);
     }*/
 
     @Operation(summary = "회원 탈퇴")
-    @DeleteMapping(path = "/{id}")
-    public void deleteMember(
-            @PathVariable("id") Long memberId
+    @DeleteMapping(path = "/{member-id}")
+    public ResponseEntity<Void> deleteMember(
+            @PathVariable("member-id") Long memberId
     ){
         memberService.deleteMember(memberId);
+        return ResponseEntity.noContent().build();
     }
 
 }
