ab762c7ab1d8da4a7dcedaf33c80f751668e4e31
jwpark1211 <1211abc@naver.com>
2025-04-22 16:46:38 +0900

    refactor: Test단에 @EnableScheduling 추가

--- src/test/java/capstone/bookitty/BookittyApplicationTests.java
+++ src/test/java/capstone/bookitty/BookittyApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@EnableScheduling
 class BookittyApplicationTests {
 
 	@Test
