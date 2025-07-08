ce6e9c2463f95489cf419c3da28803ad75ae090b
jwpark1211 <1211abc@naver.com>
2025-07-08 20:57:12 +0900

    refactor: PasswordEncoder DI 방식 변경

--- src/main/java/capstone/bookitty/global/authentication/SecurityPasswordEncoder.java
+++ src/main/java/capstone/bookitty/global/authentication/SecurityPasswordEncoder.java
@@ -1,17 +1,15 @@
 package capstone.bookitty.global.authentication;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Primary
 @Component
+@RequiredArgsConstructor
 public class SecurityPasswordEncoder implements PasswordEncoder {
-    private final BCryptPasswordEncoder delegate;
 
-    public SecurityPasswordEncoder() {
-        this.delegate = new BCryptPasswordEncoder();
-    }
+    private final org.springframework.security.crypto.password.PasswordEncoder delegate;
 
     @Override
     public String encode(String rawPassword) {
