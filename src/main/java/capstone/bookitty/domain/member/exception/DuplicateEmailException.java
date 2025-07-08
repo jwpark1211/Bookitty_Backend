d160445b657bc4d83dc520c373a32224114ebfbb
jwpark1211 <1211abc@naver.com>
2025-07-08 20:58:27 +0900

    refactor : error 메시지 변경

--- src/main/java/capstone/bookitty/domain/member/exception/DuplicateEmailException.java
+++ src/main/java/capstone/bookitty/domain/member/exception/DuplicateEmailException.java
@@ -2,6 +2,6 @@ package capstone.bookitty.domain.member.exception;
 
 public class DuplicateEmailException extends RuntimeException {
    public DuplicateEmailException(String email){
-       super("이미 사용 중인 이메일입니다:" + email);
+       super("email is already in use:" + email);
    }
 }
