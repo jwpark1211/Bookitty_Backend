9d8db8cbf5ec3baa11b48c916fc10f58077079d2
jwpark1211 <1211abc@naver.com>
2025-07-18 16:14:26 +0900

    chore : 로그 제거

--- src/main/java/capstone/bookitty/domain/member/api/AuthApi.java
+++ src/main/java/capstone/bookitty/domain/member/api/AuthApi.java
@@ -3,12 +3,11 @@ package capstone.bookitty.domain.member.api;
 import capstone.bookitty.domain.member.api.dto.MemberLoginRequest;
 import capstone.bookitty.domain.member.api.dto.tokenDto.TokenRequest;
 import capstone.bookitty.domain.member.api.dto.tokenDto.TokenResponse;
-import capstone.bookitty.domain.member.application.AuthService;
+import capstone.bookitty.domain.member.application.authApplication.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "auth", description = "인증 관련 api 입니다.")
 @RestController
-@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/members")
 public class AuthApi {
 
     private final AuthService authService;
@@ -29,11 +27,10 @@ public class AuthApi {
     public ResponseEntity<TokenResponse> login(
             @RequestBody @Valid MemberLoginRequest request
     ) {
-        log.info("로그인 요청 - email: {}", request.email());
-        TokenResponse response = authService.login(request);
 
-        log.info("로그인 완료 - 회원 ID: {}, 이름: {}", response.idx(), response.name());
+        TokenResponse response = authService.login(request);
         return ResponseEntity.ok(response);
+
     }
 
     @Operation(summary = "토큰 재발행")
@@ -41,21 +38,20 @@ public class AuthApi {
     public ResponseEntity<TokenResponse> reissue(
             @RequestBody @Valid TokenRequest request
     ) {
-        log.info("토큰 재발행 요청");
-        TokenResponse response = authService.reissue(request);
 
-        log.info("토큰 재발행 완료");
+        TokenResponse response = authService.reissue(request);
         return ResponseEntity.ok(response);
+
     }
 
     @Operation(summary = "로그아웃")
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestBody TokenRequest tokenRequest) {
-        log.info("로그아웃 요청");
-        authService.logout(tokenRequest);
+    public ResponseEntity<Void> logout(
+            @RequestBody TokenRequest tokenRequest) {
 
-        log.info("로그아웃 완료");
+        authService.logout(tokenRequest);
         return ResponseEntity.noContent().build();
+
     }
 
 }
